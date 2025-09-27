f86927f709b81978edf9084479b6258fbdb10b9c
Frankduff1505 <frankduff1505@gmail.com>
2025-09-27 19:30:14 +0530

    Updated by Frank

--- AutomationDemoGit/src/demoPackage/DemoClass.java
+++ AutomationDemoGit/src/demoPackage/DemoClass.java
@@ -13,7 +13,9 @@ public class DemoClass {
 		System.out.println("Java8");
 		System.out.println("Java6");
 		System.out.println("Java9");
-		System.out.println("Java11");
+		for(int i=0;i<10;i++) {
+			
+		}
 
 	}
 
